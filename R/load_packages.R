70deebe0ac51d850dfb2dac7d11f11c41b6a1274
audhalbritter <me@audhalbritter.com>
2021-06-25 11:31:28 +0200

    add common library file

---
+++ R/load_packages.R
@@ -0,0 +1,13 @@
+#### LOAD PACKAGES ####
+
+library("tidyverse")
+library("lubridate")
+library("stringi")
+library("readxl")
+library("writexl")
+#devtools::install_github("Between-the-Fjords/dataDownloader")
+library("dataDownloader")
+
+
+# Stuff
+pn <- . %>% print(n = Inf)
