ff1e9f12de9c6b730c9b720191a439d95dba4b2e
audhalbritter <me@audhalbritter.com>
2022-10-18 13:23:17 +0200

    fixes in create metadata function

--- R/create_metadata.R
+++ R/create_metadata.R
@@ -2,13 +2,25 @@
 
 source("R/load_packages.R")
 
-siteID = c("Arhelleren", "Alrust", "Fauske","Gudmedalen", "Hogsete", "Lavisdalen", "Ovstedal", "Rambera", "Skjelingahaugen", "Ulvehaugen", "Veskre", "Vikesland")
+siteID = c("Arhelleren",
+           "Alrust",
+           "Fauske",
+           "Gudmedalen",
+           "Hogsete",
+           "Lavisdalen",
+           "Ovstedalen",
+           "Rambera",
+           "Skjelingahaugen",
+           "Ulvehaugen",
+           "Veskre",
+           "Vikesland")
 
 blockID = c(1:4)
 
 treatment = c("C", "F", "G", "B", "FB", "GB", "GF", "FGB")
 
 funder_metadata <- crossing(siteID, blockID, treatment) %>%
-  mutate(plotID = paste(str_sub(siteID, 1, 3), blockID, treatment, sep = "_"))
+  mutate(plotID = paste(str_sub(siteID, 1, 3), blockID, treatment, sep = "_"),
+         blockID = paste0(str_sub(siteID, 1, 3), blockID))
 
-write_csv(funder_metadata, "Funder_2021_metadata.csv")
+write_csv(funder_metadata, "Funder_2022_metadata.csv")
